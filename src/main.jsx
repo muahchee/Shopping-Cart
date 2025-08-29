591f0b2ed16a8afc9340e886913ad96845a7c1e1
muahchee („• ᴗ •„) <187490843+muahchee@users.noreply.github.com>
2025-08-29 17:26:30 +1200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import { Greeting } from "./Greeting.jsx";
+import { Robot } from "./assets/Robot.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Greeting />
+    <Robot />
+  </StrictMode>
+);
+
